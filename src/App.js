d64f9f4f2a73063b91ffd5326adde9da94970bc8
7flash-pi <82704654+7flash-pi@users.noreply.github.com>
2022-05-25 22:42:40 +0530

    final

--- src/App.js
+++ src/App.js
@@ -5,21 +5,17 @@ import img1 from './images/accessbility.png'
 import img2 from './images/product-launch.png'
 import img3 from './images/branding.png'
 import Contact from "./components/Contact";
-
-
-
 function App() {
   return (
     <div className="App">
       <Header />
+      <div className="about">
       <About  src={img1}  info={'Making ur Products accessible'}/>
       <About  src={img2}  info={'platform to launch ur product'}/>
       <About  src={img3}  info={'branding ur product'}/>
+      </div>
       <Contact />
-
-
-
-    </div>
+      </div>
   );
 }
 
